154aed3915aef7dbf9ee4cdd5c86038efb224bae
ukigumo-shiina3 <daisukeyao@outlook.jp>
2021-08-09 17:11:02 +0900

    開発環境作成と本番向けのビルド設定

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,9 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-    cssnano: {},
-  },
-}
+module.exports = (ctx) => { // ctx→context
+  return {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+      cssnano: ctx.env === "production" ? {} : false, // ctx.env→node.envの値が入る
+    },
+  };
+};
