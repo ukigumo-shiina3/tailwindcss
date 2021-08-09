06424f6339360a5c47a61cd76d87b0112150e816
ukigumo-shiina3 <daisukeyao@outlook.jp>
2021-08-09 16:44:24 +0900

    purgeによる未使用classの削除

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  purge: [],
+  purge: ["./index.html"],
+  // index.htmlで使われているtailwind cssのクラス以外を削除(未使用クラスの削除)
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
